5974b52a50452572d079edc13144e8a5f5e7e693
duck zee <duyqn2002@gmail.com>
2022-12-23 19:28:53 +0700

    temp change

--- frontend/src/Error/Error3/DoiTacNF3.js
+++ frontend/src/Error/Error3/DoiTacNF3.js
@@ -1,12 +1,48 @@
 import React from 'react'
 
 function DoiTacNF3() {
+  const [list,setList]=useState([])
+  const [url,setUrl]=useState('')
+
+  useEffect(()=>{
+
+    apiCaller(url,'POST',null).then(res=>{
+      setList(res.data)
+    })
+
+  },[url])
+
+  function Handle(){
+
+    var thongbao = document.getElementById('thongbao')
+    thongbao.innerHTML='Thêm món thành công'
+
+    var monan= {list};
+
+    const matd= document.getElementById('matd').value
+
+
+    const tenmon= document.getElementById('tenmon').value
+
+
+    const mota= document.getElementById('mota').value
+
+    const gia= document.getElementById('gia').value
+
+    const tinhtrang= document.getElementById('tinhtrang').value
+
+
+    setUrl(`ThemMonAn/${matd}/${tenmon}/${gia}`)
+
+  }
+
+
   return (
 
     <div>
 
       <label for="name" className='error-lable'>Nhập tên món ăn:</label>
-      <input type="text" id="tenmon" name="temon" required
+      <input type="text" id="tenmon" name="tenmon" required
        minlength="4" maxlength="8" size="10" className='error-lable'/>
 
       <label for="name" className='error-lable'>Nhập mã thực đơn:</label>
@@ -26,6 +62,8 @@ function DoiTacNF3() {
        minlength="4" maxlength="30" size="10" className='error-lable'/>
       
       <input type="submit" name="" value="Submit" className='error-lable'/>
+
+      <p id='thongbao'></p>
     </div>
 
   )
